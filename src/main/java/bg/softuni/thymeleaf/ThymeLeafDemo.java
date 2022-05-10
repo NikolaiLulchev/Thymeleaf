b43b71420f588c77c7a3db83145e3349c5b2f2b8
NikolaiLulchev <nikolai.lulchev@gmail.com>
2022-05-10 11:44:04 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/thymeleaf/ThymeLeafDemo.java
@@ -0,0 +1,24 @@
+package bg.softuni.thymeleaf;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+public class ThymeLeafDemo {
+    public static void main(String[] args) {
+        ITemplateResolver resolver = new ClassLoaderTemplateResolver();
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(resolver);
+
+        Context context = new Context();
+        context.setVariable("personToGreet", "Niki");
+
+        String result = templateEngine.process("test.html",context);
+
+        System.out.println(result);
+
+
+    }
+}
